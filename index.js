5712d1b45c562f42f35d9ba0487c4adf37cb7af1
Junior Rosa <acsrosajunior@gmail.com>
2022-11-01 16:35:25 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de altura e seu IMC é de 25.925925925925924
+Luiz Otávio nasceu em 1980
+*/
+const nome = 'Junior Silva';
+const sobreNome = 'Rosa';
+const idade = 42;
+const peso = 112;
+const alturaEmM = 1.68;
+let indiceDeMassaCorporal = peso / (alturaEmM * alturaEmM)
+let anoDeNascimento = 2022 - idade
